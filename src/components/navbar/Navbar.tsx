02c8a293bdd9ac947ecca1b6cace06a36de8a87a
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2024-12-02 11:17:25 +0300

    feat: Добавлена функция logout для пользователя (#7)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import {Button} from "@consta/uikit/Button";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import AppPage from "../../const.ts";
 import './Navbar.css'
 import {Layout} from "@consta/uikit/Layout";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../store/user-store/UserStore.tsx";
+import {logout} from "../../store/user-store/UserSlice.tsx";
 
 
 const Navbar = () => {
@@ -13,38 +14,60 @@ const Navbar = () => {
         return state.user;
     })
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
 
     return (
     <Layout>
     <div className="navbar">
         <div className="navbar-container">
-            {user.isAuthenticated ? (<div className="navbar-group left">
+            <div className="navbar-group left">
                 <NavLink to={AppPage.main} className={"navbar-button"}>
                     <Button label={"Главная страница"}></Button>
                 </NavLink>
-                <NavLink to={AppPage.services} className={"navbar-button"}>
-                    <Button label={'Услуги компании'}></Button>
-                </NavLink>
-            </div>) : (
-                <h3>Вы не зарегистрированы</h3>
-            )}
-            <div className="navbar-group right">
-                {!user.isAuthenticated ? (
-                    <NavLink to={AppPage.login} className={"navbar-button"}>
-                        <Button label={'Вход'} ></Button>
+                {user.isAuthenticated ? (
+
+                    <NavLink to={AppPage.services} className={"navbar-button"}>
+                        <Button label={'Услуги компании'}></Button>
                     </NavLink>
                 ) : (
-                    <NavLink to={AppPage.userinfo + user.id} className={"navbar-button"}>
-                        <Button label="Профиль" ></Button>
-                    </NavLink>
+                    <h3>Вы не зарегистрированы</h3>
                 )}
             </div>
+                <div className="navbar-group right">
+                    {!user.isAuthenticated ? (
+                        <NavLink to={AppPage.login} className={"navbar-button"}>
+                            <Button label={'Вход'}></Button>
+                        </NavLink>
+                    ) : (
+                        <>
+                            <NavLink to={AppPage.userinfo + user.id} className={"navbar-button"}>
+                                <Button label="Профиль"></Button>
+                            </NavLink>
+                            <Button
+                                label={'Выход'}
+                                className={"navbar-button"}
+                                onClick={() => {
+                                    navigate('')
+                                    dispatch(logout({
+                                            id: null,
+                                            accessToken: ''
+                                        }
+                                    ))
 
+
+                                }}
+                            />
+                        </>
+                    )}
+                </div>
+
+            </div>
+            <hr/>
         </div>
-        <hr/>
-    </div>
     </Layout>
-    )
+)
 }
 
 export default Navbar
