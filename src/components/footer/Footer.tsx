f7ede3b2d3564337acc126257a5571542961c4c7
Artem09076 <grigoranartem592@yandex.ru>
2024-12-03 12:12:04 +0300

    fix: Исправлено отображение кнопки профиля

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,8 +2,12 @@ import "./Footer.css";
 import AppPage from "../../const.ts";
 import {Button} from "@consta/uikit/Button";
 import {NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store/user-store/UserStore.tsx";
 
 const Footer = () => {
+    const user = useSelector((state: RootState) => state.user);
+
     return (
         <footer className="footer">
             <div className="footer-container">
@@ -11,6 +15,15 @@ const Footer = () => {
                     <NavLink to={AppPage.main} className={"navbar-button"}>
                         <Button label={"Главная страница"}></Button>
                     </NavLink>
+                    {user.isAuthenticated ? (
+
+                        <NavLink to={AppPage.services} className={"navbar-button"}>
+                            <Button label={'Услуги компании'}></Button>
+                        </NavLink>
+                    ): (
+                        <>
+                        </>
+                    )}
                 </div>
                 <div className="footer-right">
                     <ul className="footer-links">
--- src/store/user-store/UserSlice.tsx
+++ src/store/user-store/UserSlice.tsx
@@ -4,12 +4,14 @@ type UserState = {
     isAuthenticated: boolean;
     accessToken: string;
     id: number | null;
+    fullName: string;
 }
 
 const initialState: UserState = {
     isAuthenticated: false,
     accessToken: '',
     id: null,
+    fullName: ''
 }
 
 const UserSplice = createSlice({
@@ -20,9 +22,11 @@ const UserSplice = createSlice({
             state.accessToken = action.payload.accessToken;
             state.id = action.payload.id;
             state.isAuthenticated = true;
+            state.fullName = action.payload.fullName;
         },
         logout: (state, action) => {
             state.accessToken = action.payload.accessToken ;
+            state.fullName = '';
             state.id = action.payload.id;
             state.isAuthenticated = false;
         }
